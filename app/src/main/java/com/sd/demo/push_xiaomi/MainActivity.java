d5f82c68addfd13f91e416b0df7d9d7bac79873d
zjun <565061763@qq.com>
2021-01-21 17:20:00 +0800

    first commit

---
+++ app/src/main/java/com/sd/demo/push_xiaomi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sd.demo.push_xiaomi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
